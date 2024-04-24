255bc8488b7d092b874fde7b18f5a2bc918d5e02
191484 <191484@buas.nl>
2024-04-25 01:35:19 +0200

    [ADDED] DeveloperSettings class, for project settings

---
+++ Plugins/AchievementSystem/Source/AchievementSystem/Public/AchievementSystemSettings.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "AchievementSystemSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACHIEVEMENTSYSTEM_API UAchievementSystemSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+	
+};
