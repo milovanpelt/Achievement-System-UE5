c54e6ccc2f0d3650395b550214d0300762f22d16
191484 <191484@buas.nl>
2024-04-26 17:56:59 +0200

    [ADDED] Subsystem for achievements

---
+++ Plugins/AchievementSystem/Source/AchievementSystem/Private/AchievementSubsystem.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AchievementSubsystem.h"
+
+void UAchievementSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+}
+
+void UAchievementSubsystem::Deinitialize()
+{
+}
